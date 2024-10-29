528a0a376c20fc239cd3ae28d9d77fc53bfda3f5
Isabella Cascais <isalelis31@gmail.com>
2024-10-28 22:11:55 -0300

    Organização das classes em pacotes

--- src/view/cadastroVIEW.java
+++ src/view/cadastroVIEW.java
@@ -1,3 +1,10 @@
+package view;
+
+
+import view.listagemVIEW;
+import dto.ProdutosDTO;
+import dao.ProdutosDAO;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
