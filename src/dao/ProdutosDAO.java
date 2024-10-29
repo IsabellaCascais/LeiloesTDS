96fcf48029564a7c610f842c7ea756eb3ac10e74
Isabella Cascais <isalelis31@gmail.com>
2024-10-28 23:05:35 -0300

    Função Salvar funcionando

--- src/dao/ProdutosDAO.java
+++ src/dao/ProdutosDAO.java
@@ -4,42 +4,51 @@ package dao;
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author Adm
  */
-
+import conexao.conectaDAO;
 import dto.ProdutosDTO;
 import java.sql.PreparedStatement;
 import java.sql.Connection;
 import javax.swing.JOptionPane;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-
 public class ProdutosDAO {
-    
+
+    private conectaDAO conexao;
     Connection conn;
-    PreparedStatement prep;
+    PreparedStatement st;
     ResultSet resultset;
     ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-    
-    public void cadastrarProduto (ProdutosDTO produto){
-        
-        
-        //conn = new conectaDAO().connectDB();
-        
-        
+
+    public ProdutosDAO() {
+
+        this.conexao = new conectaDAO();
+        this.conn = this.conexao.connectDB();
     }
-    
-    public ArrayList<ProdutosDTO> listarProdutos(){
-        
+
+    public void cadastrarProduto(ProdutosDTO produto) throws SQLException {
+
+        try {
+            st = conn.prepareStatement("INSERT INTO produtos(nome, valor, status) VALUES(?,?,?)");
+            st.setString(1, produto.getNome());
+            st.setInt(2, produto.getValor());
+            st.setString(3, produto.getStatus());
+
+            st.execute();
+
+        } catch (SQLException ex) {
+            System.out.println("Erro ao conectar: " + ex.getMessage());
+        }
+    }
+
+    public ArrayList<ProdutosDTO> listarProdutos() {
+
         return listagem;
     }
-    
-    
-    
-        
-}
 
+}
